12e0448bd898c94e56f56dcb69f255ea01436be5
migdracios <migdracios@gmail.com>
2022-05-15 19:32:08 +0900

    추가 - flask 기본 뼈대 세팅

---
+++ turtlegram/backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return jsonify({'message': 'success'})
+
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    print(request.form)
+    print(request.form.get('id'))
+    print(request.form.data)
+    return jsonify({'msg': 'True'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
