4e09abafa2b828a26f6b27aea97c0dafd7729a2a
migdracios <migdracios@gmail.com>
2022-05-15 21:14:38 +0900

    수정 - 비동기 함수 주석

--- turtlegram/frontend/api.js
+++ turtlegram/frontend/api.js
@@ -6,9 +6,11 @@ async function handleSignin() {
         password: document.getElementById('floatingPassword').value
     }
 
+
+    // 언제 들어올지 모르니까 일단 대기
     const response = await fetch('http://127.0.0.1:5000/signup', {
         method: 'POST',
-        body: signupData
+        body: JSON.stringify(signupData)
     })
     console.log(response)
 
