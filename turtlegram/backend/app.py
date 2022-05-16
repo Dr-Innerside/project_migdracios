df2b87cf279095b0e3d8cecfe4d5c3667f28a576
migdracios <migdracios@gmail.com>
2022-05-16 23:44:44 +0900

    추가 - 회원가입 API 데이터베이스 저장

--- turtlegram/backend/app.py
+++ turtlegram/backend/app.py
@@ -1,10 +1,14 @@
 import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+client = MongoClient('localhost', 27017)
+db = client.nabacamp
 
 
 @app.route('/')
@@ -17,10 +21,16 @@ def sign_up():
     # print(request.form)
     # print(request.form.get('id'))
     # print(request.data)
+    # print(request.data)
     data = json.loads(request.data)
-    print(f'data={data}')
-    print(data.get('email'))
-    print(data.get('password'))
+    id_receive = data.get('id')
+    pw_receive = data.get('pw')
+    # print(f'data={data}')
+    doc = {
+        'id': id_receive,
+        'pw': pw_receive
+    }
+    db.user.insert_one(doc)
     return jsonify({'msg': 'True'})
 
 
