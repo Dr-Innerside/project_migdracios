b4d959e9997846fc48ecbc8f9f2277c9b13566a8
migdracios <migdracios@gmail.com>
2022-05-20 19:47:58 +0900

    추가 - 이미지 업로드 API

--- cnn/cat_and_dog/app.py
+++ cnn/cat_and_dog/app.py
@@ -1,4 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+# from keras.applications import ResNet50
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+
 
 app = Flask(__name__)
 
@@ -6,6 +23,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello dogs'
+
+
+@app.route('/upload', methods=["POST"])
+def upload_image():
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    now = datetime.now()
+    timestamp = now.strftime('%Y%m%d_$H$M$S')
+    filename = f'{timestamp}.{extension}'
+    save_to = f'img/{filename}'
+    file.save(save_to)
+    return save_to
 
 
 if __name__ == '__main__':
