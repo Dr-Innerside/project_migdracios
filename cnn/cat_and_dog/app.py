8651da2ac58bccdd771ce2a6f0a751b79d6eb8df
migdracios <migdracios@gmail.com>
2022-05-20 19:59:59 +0900

    시도 - 이미지 업로드 jquery

--- cnn/cat_and_dog/app.py
+++ cnn/cat_and_dog/app.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template, request
 from datetime import datetime
 
 
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Dropout
-from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import load_model
-# from keras.applications import ResNet50
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
+# from tensorflow.keras.models import Model
+# from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+# from tensorflow.keras.optimizers import Adam, SGD
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from tensorflow.keras.models import load_model
+# # from keras.applications import ResNet50
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import OneHotEncoder
 
 
 app = Flask(__name__)
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello dogs'
+    return render_template({'index.html'})
 
 
 @app.route('/upload', methods=["POST"])
