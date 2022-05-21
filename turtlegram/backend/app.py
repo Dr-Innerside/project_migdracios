79c0ad3e7d34aeb869db8e9ccfb7071a83d02cfc
migdracios <migdracios@gmail.com>
2022-05-21 14:15:36 +0900

    수정 - flask 기본 뼈대 수정

--- turtlegram/backend/app.py
+++ turtlegram/backend/app.py
@@ -1,5 +1,4 @@
 import json
-from socket import MsgFlag
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -20,28 +19,16 @@ def hello_world():
 
 @app.route('/signup', methods=['POST'])
 def sign_up():
-    # print(request.form)
-    # print(request.form.get('id'))
-    # print(request.data)
-    # print(request.data)
-
-    # request
     data = json.loads(request.data)
 
-    # api progress
-
-    # 요청값 변수 담기
     id_receive = data.get('id')
     pw_receive = data.get('pw')
 
-    # 비밀번호 암호화
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    # 아이디 일치여부 조회
     if db.user.find({'id': id_receive}):
         msg = '이미 존재하는 아이디입니다.'
     else:
-        # 몽고디비 컬렉션 저장
         doc = {
             'id': id_receive,
             'pw': pw_hash
