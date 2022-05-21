f12108f3131da8f100b80afa5db03e143698793d
migdracios <migdracios@gmail.com>
2022-05-21 15:14:27 +0900

    수정 - 회원가입 조건 순서 및 로컬 DB 컬렉션 네임

--- turtlegram/backend/app.py
+++ turtlegram/backend/app.py
@@ -12,11 +12,12 @@ cors = CORS(app, resources={r"*": {"origins": "*"}})
 SECRET_KEY = 'WIZARD1993'
 
 client = MongoClient('localhost', 27017)
-db = client.nabacamp
+db = client.turtlegram
 
-bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['BCRYPT_LEVEL'] = 10
+bcrypt = Bcrypt(app)
+
 
 
 @app.route('/')
@@ -31,17 +32,18 @@ def sign_up():
     id_receive = data.get('id')
     pw_receive = data.get('pw')
 
-    pw_hash = bcrypt.gererate_password_hash(pw_receive)
+    pw_hash = bcrypt.generate_password_hash(pw_receive)
 
     if db.user.find({'id': id_receive}):
-        msg = '이미 존재하는 아이디입니다.'
-    else:
         doc = {
             'id': id_receive,
             'pw': pw_hash
         }
         db.user.insert_one(doc)
         msg = '회원가입이 완료되었습니다.'
+        
+    else:
+        msg = '이미 존재하는 아이디입니다.'
     return jsonify({'msg': msg})
 
 @app.route('/login', methods=['POST'])
