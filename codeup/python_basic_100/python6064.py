fc208828a5c9a739e6f43777ba91c9ed05afe232
migdracios <migdracios@gmail.com>
2022-05-23 07:10:35 +0900

    수정 - 함수 호출

--- codeup/python_basic_100/python6064.py
+++ codeup/python_basic_100/python6064.py
@@ -3,6 +3,9 @@
 # 입력된 세 정수 a, b, c 중 가장 작은 값을 출력하는 프로그램을 작성해보자.
 # 단, 3항 연산을 사용한다.
 
+from cgitb import small
+
+
 a, b, c = map(int, input().split())
 #   --- 세 값 중 가장 작은 값을 뽑기 위해서는
 #   --- 조건문으로 일일이 비교해야 할까?
@@ -28,5 +31,6 @@ def small_number(num1, num2, num3):
         smallest = num2 if num2 < num3 else num3
         return smallest
 
-    # result = f'답은 {big_number(a,b,c)}'
-    print(smallest)
+
+smallest = small_number(num1, num2, num3)
+print(smallest)
