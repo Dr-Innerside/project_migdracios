1d8f18c769cf540fc1123f6a6cd938880fe7da21
migdracios <migdracios@gmail.com>
2022-05-24 07:50:51 +0900

    완료 - 6076 : for in range 구문을 사용하여 반복문 출력

---
+++ codeup/python_basic_100/python6076.py
@@ -0,0 +1,16 @@
+# 6076
+
+# 정수(0 ~ 100) 1개를 입력받아 0부터 그 수까지 순서대로 출력해보자.
+
+# 예시
+# n = int(input())
+# for i in range(n+1) :
+#   print(i)
+
+
+# -- 값 입력 --
+num = int(input())
+
+# -- 입력 값까지 (예: 6, 0부터 6까지 총 7개)
+for i in range(num+1):
+    print(i)
