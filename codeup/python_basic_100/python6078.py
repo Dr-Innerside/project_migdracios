5327da3d2a724336bc216629a16d71a978f0c42c
migdracios <migdracios@gmail.com>
2022-05-26 07:27:41 +0900

    완료 - 6078 : 처음부터 q를 입력하는 로직 추가

--- codeup/python_basic_100/python6078.py
+++ codeup/python_basic_100/python6078.py
@@ -3,8 +3,12 @@
 # 영문 소문자 'q'가 입력될 때까지
 # 입력한 문자를 계속 출력하는 프로그램을 작성해보자.
 
+
 mychar = input()
 
+if mychar == 'q':
+        print(mychar)
+        
 while mychar != 'q' :
     print(mychar)
     mychar = input()
@@ -12,3 +16,9 @@ while mychar != 'q' :
     if mychar == 'q':
         print(mychar)
         break
+
+
+
+# ---- LOGIC ----
+# 1. 문자 하나를 입력받음
+# 2. 입력한 문자가 소문자 q가 아니라면 q가 나올때까지 입력한 문자를 출력하기
