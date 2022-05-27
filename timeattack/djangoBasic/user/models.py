e3f9bef33af890f5646701d45c8d451f168b74c7
migdracios <migdracios@gmail.com>
2022-05-27 18:11:58 +0900

    TRY - 클라이언트와 register 연결해 회원가입 기능

--- timeattack/djangoBasic/user/models.py
+++ timeattack/djangoBasic/user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class UserModel(models.Model):
-#     def __init__(self, username, password):
-#         self.username = models.CharField(max_length=20, null=False)
-#         self.password = models.CharField(max_length=256, null=False)
+    def __init__(self, email, password):
+        self.email = models.CharField(max_length=20, null=False)
+        self.password = models.CharField(max_length=256, null=False)
 
-    username = models.CharField(max_length=20, null=False)
-    password = models.CharField(max_length=256, null=False)
+    # email = models.CharField(max_length=20, null=False)
+    # password = models.CharField(max_length=256, null=False)
