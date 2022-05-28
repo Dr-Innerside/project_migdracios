387835ecaf96d48c36c9d0c11a73cb51d53a2c13
migdracios <migdracios@gmail.com>
2022-05-28 21:27:27 +0900

    ADD - 회원가입, 로그인 뼈대 코드 작성

--- nbcamp/lectures/django_basic/myFirstSns/user/views.py
+++ nbcamp/lectures/django_basic/myFirstSns/user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def sign_up_view(request):
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        
+        return ''
+
+
+def sign_in_view(request):
+    return render(request, 'user/signin.html')
