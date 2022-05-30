f601a75c79c68de962245232e772ea62f8224fd3
migdracios <migdracios@gmail.com>
2022-05-30 20:01:30 +0900

    FIX - 깃허브 업로드 세팅

--- nbcamp/lectures/django_basic/myFirstSns/myFirstSns/settings.py
+++ nbcamp/lectures/django_basic/myFirstSns/myFirstSns/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z@!1+cm^*qduw(@_zwuep&om@yvsiin%d_21w#-*4(uw@a-j(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
