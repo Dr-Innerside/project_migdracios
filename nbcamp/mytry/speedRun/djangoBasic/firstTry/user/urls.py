56f89a4edc1526ff0a0d800f96a86ad456ae25d2
migdracios <migdracios@gmail.com>
2022-05-30 20:38:45 +0900

    Add - 회원가입/로그인 렌더 url 추가 작성

--- nbcamp/mytry/speedRun/djangoBasic/firstTry/user/urls.py
+++ nbcamp/mytry/speedRun/djangoBasic/firstTry/user/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls'))
+    path('', include('user.urls')),
+    path('sign-up/', views.sign_up_view, name='sign_up_view'),
+    path('sign-in/', views.sign_in_view, name='sign_in_view'),
 ]
