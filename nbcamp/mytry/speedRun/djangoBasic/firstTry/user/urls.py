69a7039b50937b4c8b5831b2a813200ddcfaab25
migdracios <migdracios@gmail.com>
2022-05-30 20:40:31 +0900

    Fix - 프로젝트 url에 앱 url을 넣어 접근할 수 있도록 코드 위치 수정

--- nbcamp/mytry/speedRun/djangoBasic/firstTry/user/urls.py
+++ nbcamp/mytry/speedRun/djangoBasic/firstTry/user/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user.urls')),
     path('sign-up/', views.sign_up_view, name='sign_up_view'),
     path('sign-in/', views.sign_in_view, name='sign_in_view'),
 ]
