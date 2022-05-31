1f0ce2a54889d9d7b891f943f111779a91147818
migdracios <migdracios@gmail.com>
2022-05-31 09:45:23 +0900

    Add - 새 게시글 작성하기 POST API 작성부터 연결까지 시도하기

--- timeattack/djangoBlog/blog/views.py
+++ timeattack/djangoBlog/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def new_article(request):
+    if request.method == 'GET':
+        render(request, 'new.html')
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        content = request.POST.get('content')
+
+        print(f'title -> {title}')
+        print(f'category -> {category}')
+        print(f'content -> {content}')
+
+        return ''
