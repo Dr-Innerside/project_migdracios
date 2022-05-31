bdaf41b5060145fb21a7cff4fff23df2b2240319
migdracios <migdracios@gmail.com>
2022-05-31 09:59:05 +0900

    Fix - url 위치 이동

--- timeattack/djangoBlog/blog/urls.py
+++ timeattack/djangoBlog/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/new', views.new_article, name='new-article'),
+    path('new/', views.new_article, name='new-article'),
 ]
