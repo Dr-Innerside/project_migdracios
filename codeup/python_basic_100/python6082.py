ebcc30e3a80a67151311b22e3e9a299b6d9abd03
migdracios <migdracios@gmail.com>
2022-06-02 07:38:30 +0900

    완료 - 6082 : 나머지 값을 수정하여 13.16 같은 값도 포함하도록 변경

--- codeup/python_basic_100/python6082.py
+++ codeup/python_basic_100/python6082.py
@@ -16,7 +16,7 @@
 
 callNum = int(input())
 for i in range(1, callNum+1):
-    if i%3 == 0:
+    if i%10 == 3 or i%10 == 6 or i%10 == 9:
         print('X', end=' ')
     else:
         print(i, end=' ')
