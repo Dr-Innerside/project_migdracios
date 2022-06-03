fa2a08cd59f7dee77422a3cd2f843fac2c8b5cd7
migdracios <migdracios@gmail.com>
2022-06-03 16:37:53 +0900

    Add - 카테고리 데이터 리스폰스

--- timeattack/djangoCoffe/product/views.py
+++ timeattack/djangoCoffe/product/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from .models import Category, Drink
 
 # Create your views here.
 def first(request):
-    return render(request, 'category.html')
+    categories = Category.objects.all()
+
+    return render(request, 'category.html', {'categories': categories})
