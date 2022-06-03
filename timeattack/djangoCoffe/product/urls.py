58e2ce69d6066ba446faf8fa48408fc0b5df8c51
migdracios <migdracios@gmail.com>
2022-06-03 16:50:16 +0900

    Feat - 카테고리에 해당하는 드링크 정보 보여주기

--- timeattack/djangoCoffe/product/urls.py
+++ timeattack/djangoCoffe/product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.first, name='first'),
+    path('drink/<int:id>', views.drink, name='drink'),
 ]
--- timeattack/djangoCoffe/product/views.py
+++ timeattack/djangoCoffe/product/views.py
@@ -6,3 +6,12 @@ def first(request):
     categories = Category.objects.all()
 
     return render(request, 'category.html', {'categories': categories})
+
+def drink(request, id):
+    if request.method == 'GET':
+        print(f'드링크로 넘어가랏!')
+        category = Category.objects.get(id=id)
+        drinks = Drink.objects.filter(category=category)
+        print(f'해당하는 드링크는! {drinks}')
+        return render(request, 'drink.html', {'drinks':drinks})
+
