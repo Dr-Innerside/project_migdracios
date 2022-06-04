ee4073db4771093356715fc8e7a4443a1093461a
migdracios <migdracios@gmail.com>
2022-06-04 18:45:22 +0900

    Feat - 회원가입, 로그인 API 뼈대 및 URL 연결

--- nbcamp/mytry/speedRun/djangoBasic/fourthTry/user/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/fourthTry/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def sign_up_view(request):
+    return HttpResponse("SIGN UP")
+
+def sign_in_view(request):
+    return HttpResponse('SIGN IN')
