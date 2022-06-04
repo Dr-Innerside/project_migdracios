84ec6749b86116c56c30115349f95fccc3d67bf7
migdracios <migdracios@gmail.com>
2022-06-04 18:49:34 +0900

    Feat - 사용자 조회, 비밀번호 조회가 포함된 회원가입 API

--- nbcamp/mytry/speedRun/djangoBasic/fourthTry/user/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/fourthTry/user/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserModel
+
 
 # Create your views here.
 def sign_up_view(request):
-    return HttpResponse("SIGN UP")
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        bio = request.POST.get('bio')
+
+        exist_user = UserModel.objects.filter(username=username)
+        if exist_user:
+            if password == password2:
+                new_user = UserModel()
+                new_user.username = username
+                new_user.password = password
+                new_user.bio = bio
+                new_user.save()
+            else:
+                return HttpResponse("비밀번호가 일치하지 않습니다")
+        else:
+            return HttpResponse("존재하지 않는 사용자 입니다")
+        return ''
+
+    return ''
+
 
 def sign_in_view(request):
-    return HttpResponse('SIGN IN')
+    return ''
