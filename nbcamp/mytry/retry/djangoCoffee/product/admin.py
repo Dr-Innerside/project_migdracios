9d20c55905ae8aadf53722a4b4ca4d7021fd5f83
migdracios <migdracios@gmail.com>
2022-06-05 18:03:01 +0900

    Add - 각 페이지 연결 뼈대 API

--- nbcamp/mytry/retry/djangoCoffee/product/admin.py
+++ nbcamp/mytry/retry/djangoCoffee/product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Drink, Image
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Drink)
+admin.site.register(Image)
--- nbcamp/mytry/retry/djangoCoffee/product/views.py
+++ nbcamp/mytry/retry/djangoCoffee/product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_category(request):
+    return render(request, 'category.html')
+
+def show_drink(request):
+    return render(request, 'drink.html')
+
+def show_drink_detail(request):
+    return render(request, 'drink_detail.html')
