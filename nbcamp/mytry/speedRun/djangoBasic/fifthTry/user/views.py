acdee22f25325a14a4204c665713ad754ceb0ee6
migdracios <migdracios@gmail.com>
2022-06-05 17:37:44 +0900

    Initial - 2주차, AbstractUser 활용까지 스피드런 세팅

---
+++ nbcamp/mytry/speedRun/djangoBasic/fifthTry/user/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+
+# Create your views here.
+def sign_up_view(request):
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        bio = request.POST.get('bio')
+
+        if password2 != password:
+            return render(request, 'user/signup.html')
+
+        else:
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in')
+
+
+def sign_in_view(request):
+    if request.method == 'GET':
+        return render(request, 'user/signin.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        me = auth.authenticate(request, username=username, password=password)
+        if me:
+            auth.login(request, me)
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
