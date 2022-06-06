3e0ae9f181d65643ba185470ad10859a7acc171b
migdracios <migdracios@gmail.com>
2022-06-07 00:01:47 +0900

    Feat - tweet POST API

--- nbcamp/mytry/speedRun/djangoBasic/seventhTry/tweet/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/seventhTry/tweet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import TweetModel
 
 # Create your views here.
 def home(request):
@@ -9,8 +10,17 @@ def home(request):
         return redirect('/sign-in')
 
 def tweet(request):
-    user = request.user.is_authenticated
-    if user:
-        return render(request, 'tweet/home.html')
-    else:
-        return redirect('/sign-in')
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            return render(request, 'tweet/home.html')
+        else:
+            return redirect('/sign-in')
+    if request.method == 'POST':
+        user = request.user
+        content = request.POST.get('my-content')
+        new_tweet = TweetModel()
+        new_tweet.author = user
+        new_tweet.content = content
+        new_tweet.save()
+        return redirect('/tweet')
