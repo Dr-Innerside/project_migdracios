fba2c66e1bcc70e0e8f00cc9ec64bf6d8d2001e2
migdracios <migdracios@gmail.com>
2022-06-06 23:48:24 +0900

    Feat - 회원가입 API

--- nbcamp/mytry/speedRun/djangoBasic/seventhTry/user/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/seventhTry/user/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from .models import UserModel
 
 # Create your views here.
 def sign_up_view(request):
-    return render(request, 'user/signup.html')
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        bio = request.POST.get('bio')
 
+        if password == password2:
+            exist_user = get_user_model().objects.filter(username=username, password=password)
+            if exist_user:
+                UserModel().objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in')
+            else:
+                return render(request, 'user/signup.html')
+        else:
+            return render(request, 'user/signup.html')
 def sign_in_view(request):
     return render(request, 'user/signin.html')
 
