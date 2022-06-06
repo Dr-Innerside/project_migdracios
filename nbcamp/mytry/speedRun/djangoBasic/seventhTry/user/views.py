2d59a9a5a60cc85c50682f0c1a823e942fad9fe7
migdracios <migdracios@gmail.com>
2022-06-06 23:45:02 +0900

    Add - base, signup, signin 페이지 내용 수정 및 백엔드 연결

--- nbcamp/mytry/speedRun/djangoBasic/seventhTry/user/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/seventhTry/user/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def sign_up_view(request):
-    return ''
+    return render(request, 'user/signup.html')
 
 def sign_in_view(request):
-    return ''
+    return render(request, 'user/signin.html')
 
 def sign_out(request):
     return ''
