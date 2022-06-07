a7deaf575c631024dc9440407d7b62f647729a17
migdracios <migdracios@gmail.com>
2022-06-07 11:05:34 +0900

    Add - UserModel, TweetModel Migration

--- nbcamp/mytry/speedRun/djangoBasic/eighthTry/user/models.py
+++ nbcamp/mytry/speedRun/djangoBasic/eighthTry/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class UserModel(models.Model):
+    class Meta:
+        db_table = 'my_user'
+    bio = models.CharField(max_length=500, default='')
