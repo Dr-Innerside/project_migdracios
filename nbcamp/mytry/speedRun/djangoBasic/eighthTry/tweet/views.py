8992884b5f7ea12c95b6207df51c4955d4d56d7a
migdracios <migdracios@gmail.com>
2022-06-07 11:33:57 +0900

    Add - tweet GET/POST API

--- nbcamp/mytry/speedRun/djangoBasic/eighthTry/tweet/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/eighthTry/tweet/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from .models import TweetModel
 # Create your views here.
 def home(request):
     user = request.user.is_authenticated
@@ -9,8 +9,19 @@ def home(request):
         return redirect('/sign-in')
 
 def tweet(request):
-    user = request.user.is_authenticated
-    if user:
-        return render(request, 'tweet/home.html')
-    else:
-        return redirect('/sign-in')
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        all_tweet = TweetModel.objects.all()
+        if user:
+            return render(request, 'tweet/home.html', {'tweet': all_tweet})
+        else:
+            return redirect('/sign-in')
+    elif request.method == 'POST':
+        user = request.user
+        content = request.POST.get('my-content')
+        new_tweet = TweetModel()
+        new_tweet.author = user
+        new_tweet.content = content
+        new_tweet.save()
+
+        return redirect('/tweet')
