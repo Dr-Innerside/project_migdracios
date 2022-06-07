dc153b934f65f8a0bd392ca86b798199e63ce149
migdracios <migdracios@gmail.com>
2022-06-07 11:26:03 +0900

    Add - 로그인 회원가입 페이지 로그인 조회 및 리다이렉션

--- nbcamp/mytry/speedRun/djangoBasic/eighthTry/user/views.py
+++ nbcamp/mytry/speedRun/djangoBasic/eighthTry/user/views.py
@@ -6,7 +6,11 @@ from .models import UserModel
 # Create your views here.
 def sign_up_view(request):
     if request.method == 'GET':
-        return render(request, 'user/signup.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -25,7 +29,11 @@ def sign_up_view(request):
 
 def sign_in_view(request):
     if request.method == 'GET':
-        return render(request, 'user/signin.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
