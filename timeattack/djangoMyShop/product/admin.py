6c1fc1f28c16a34e2ec7da273f1698299ba84285
migdracios <migdracios@gmail.com>
2022-06-10 16:38:52 +0900

    Add - 모델 어드민 연결

--- timeattack/djangoMyShop/product/admin.py
+++ timeattack/djangoMyShop/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserOrder, Product, Category, ProductOrder, OrderStatus
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(UserOrder)
+admin.site.register(Product)
+admin.site.register(ProductOrder)
+admin.site.register(OrderStatus)
