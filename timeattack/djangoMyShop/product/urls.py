8bf6be37829d9971d967e02b7a47c6bd39215439
migdracios <migdracios@gmail.com>
2022-06-10 17:08:14 +0900

    Feat - 카테고리에 맞는 아이템 출력 API

--- timeattack/djangoMyShop/product/urls.py
+++ timeattack/djangoMyShop/product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_cate, name='show-cate'),
+    path('category/<int:id>', views.show_products, name='show-product'),
 ]
--- timeattack/djangoMyShop/product/views.py
+++ timeattack/djangoMyShop/product/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Product
 
 # Create your views here.
 def show_cate(request):
     all_cate = Category.objects.all()
     return render(request, 'cate.html', {'categories': all_cate})
+
+def show_products(request, id):
+    if request.method =='GET':
+        all_cate = Category.objects.all()
+        product_list = Product.objects.filter(category=id)
+        return render(request, 'product.html', {'categories':all_cate, 'product_list': product_list})
+
