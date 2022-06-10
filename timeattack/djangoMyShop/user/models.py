d03c08262c359a973df1d70228a7bb09239aecda
migdracios <migdracios@gmail.com>
2022-06-10 16:20:10 +0900

    Add - UserModel 추가

--- timeattack/djangoMyShop/user/models.py
+++ timeattack/djangoMyShop/user/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class UserModel(models.Model):
     class Meta:
         db_table='my_user'
+    def __str__(self):
+        return self.email
+    email = models.CharField(max_length=30, null=False)
+    password = models.CharField(max_length=256, null=False)
