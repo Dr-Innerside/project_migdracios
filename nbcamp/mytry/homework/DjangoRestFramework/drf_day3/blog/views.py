40a4692f1006055ac01541d0a6650423c3cfc4c3
migdracios <migdracios@gmail.com>
2022-06-21 01:43:00 +0900

    ADD - DRF 4일차 게시글 시리얼라이저 셋업

--- nbcamp/mytry/homework/DjangoRestFramework/drf_day3/blog/views.py
+++ nbcamp/mytry/homework/DjangoRestFramework/drf_day3/blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+
+class ArticleView(APIView):
+    # 사용자 게시글 조회
+    def get(self, request):
+        '''
+        Serializer를 이용해
+        로그인한 사용자의
+        1. 게시글
+        2. 댓글
+        을 리턴해준다!
+        '''
+        return Response()
+    def post(self, request):
+        pass
+    def put(self, request):
+        pass
+    def delete(self, request):
+        pass
--- nbcamp/mytry/homework/DjangoRestFramework/drf_day3/user/serializers.py
+++ nbcamp/mytry/homework/DjangoRestFramework/drf_day3/user/serializers.py
@@ -2,6 +2,17 @@ from rest_framework import serializers
 
 from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel 
+from blog.models import Article as ArticleModel
+from blog.models import Comment as CommentModel
+
+
+# 사용자 댓글 시리얼라이저
+
+# 사용자 게시글 시리얼라이저
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleModel
+        fields = "__all__"
 
 
 # 사용자 상세정보 시리얼라이저
