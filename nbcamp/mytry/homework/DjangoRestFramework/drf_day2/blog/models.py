b80463f3c515d7f0affaf29ee632c27426325c23
migdracios <migdracios@gmail.com>
2022-06-20 21:30:30 +0900

    ADD - 2일차 6,7번 과제 아티클 모델 생성 및 외래키 참조 테이블 작성

--- nbcamp/mytry/homework/DjangoRestFramework/drf_day2/blog/models.py
+++ nbcamp/mytry/homework/DjangoRestFramework/drf_day2/blog/models.py
@@ -1,5 +1,8 @@
+from turtle import title
+from unicodedata import category
 from django.db import models
 
+from user.models import User
 
 
 # Create your models here.
@@ -11,3 +14,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+# 블로그 게시글 모델
+class Article(models.Model):
+    author = models.ForeignKey(User, verbose_name="글 작성자", on_delete=models.CASCADE)
+    title = models.CharField("글 제목", max_length=50)
+    category = models.ManyToManyField(Category, verbose_name="카테고리")
+    content = models.TextField("글 내용")
+
+    def __str__(self):
+        return f"{self.author.username}의 {self.title}입니다."
+    
