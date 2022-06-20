6370e31a542e05c63a55d017071eca6e1e0d3450
migdracios <migdracios@gmail.com>
2022-06-20 21:17:53 +0900

    ADD - 2일차 5번과제 카테고리 모델 생성 및 테이블 작성

--- nbcamp/mytry/homework/DjangoRestFramework/drf_day2/blog/admin.py
+++ nbcamp/mytry/homework/DjangoRestFramework/drf_day2/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Category as CategoryModel
 
 # Register your models here.
+admin.site.register(CategoryModel)
--- nbcamp/mytry/homework/DjangoRestFramework/drf_day2/blog/models.py
+++ nbcamp/mytry/homework/DjangoRestFramework/drf_day2/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+
 # Create your models here.
+
+# 블로그 카테고리 모델
+class Category(models.Model):
+    category_name = models.CharField("카테고리 이름", max_length=50)
+    category_desc = models.CharField("카테고리 설명", max_length=50)
+
+    def __str__(self):
+        return self.category_name
