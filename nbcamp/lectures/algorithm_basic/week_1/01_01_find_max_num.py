9b61e5000310d2e292ed4acdc068b499418f4dd0
migdracios <migdracios@gmail.com>
2022-07-19 10:17:58 +0900

    ADD - 지정변수를 통해 최댓값 찾기

--- nbcamp/lectures/algorithm_basic/week_1/01_01_find_max_num.py
+++ nbcamp/lectures/algorithm_basic/week_1/01_01_find_max_num.py
@@ -10,14 +10,16 @@ def find_max_num(array):
     배열 내에서 가장 큰 수를 찾아내기
     반복문으로 슬라이싱
     큰수를 반복하고 가장 큰 수를 변수로 초기화
+    
+     max_num = 0
+    for i in array:
+        #3 5 6 1 2 4
+        if i > max_num:
+            max_num = i
+    return max_num
     """
     
-    # max_num = 0
-    # for i in array:
-    #     #3 5 6 1 2 4
-    #     if i > max_num:
-    #         max_num = i
-    # return max_num
+   
     
     # 숫자를 비교하는 방식
     for num in array:
