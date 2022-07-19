91ee5e6d0b534098cfbc57c859031c0b677c48d5
migdracios <migdracios@gmail.com>
2022-07-19 10:12:00 +0900

    ADD - 0101 숫자를 비교하는 알고리즘

--- nbcamp/lectures/algorithm_basic/week_1/01_01_find_max_num.py
+++ nbcamp/lectures/algorithm_basic/week_1/01_01_find_max_num.py
@@ -4,16 +4,30 @@ input = [3, 5, 6, 1, 2, 4]
 def find_max_num(array):
     # 이 부분을 채워보세요!
     
-    # 배열 내에서 가장 큰 수를 찾아내기
-    # 반복문으로 슬라이싱
-    # 큰수를 반복하고 가장 큰 수를 변수로 초기화
-    max_num = 1
-    for i in input:
-        if i > max_num:
-            print(i)
-            max_num = i
-            print(max_num)
-    return max_num
+    """
+    나의 풀이
+    
+    배열 내에서 가장 큰 수를 찾아내기
+    반복문으로 슬라이싱
+    큰수를 반복하고 가장 큰 수를 변수로 초기화
+    """
+    
+    # max_num = 0
+    # for i in array:
+    #     #3 5 6 1 2 4
+    #     if i > max_num:
+    #         max_num = i
+    # return max_num
+    
+    # 숫자를 비교하는 방식
+    for num in array:
+        #3 5 6 1 2 4
+        for compare_num in array:
+            #3 5 6 1 2 4
+            if num < compare_num:
+                break
+        else:
+            return num
 
 
 result = find_max_num(input)
