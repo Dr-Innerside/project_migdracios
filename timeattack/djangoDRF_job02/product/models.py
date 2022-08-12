0cda1b99b7fa23f05e5f6e5a5966bb442e431341
migdracios <migdracios@gmail.com>
2022-08-12 16:51:17 +0900

    Add : 어드민을 통한 상품 데이터 추가

--- timeattack/djangoDRF_job02/product/models.py
+++ timeattack/djangoDRF_job02/product/models.py
@@ -1,5 +1,6 @@
 from cgi import print_exception
 from pydoc import describe
+from tkinter import CASCADE
 from unicodedata import name
 from django.db import models
 
@@ -11,9 +12,14 @@ class Product(models.Model):
     join_date = models.DateTimeField("도입일", auto_now_add=True)
     is_active = models.BooleanField("active 여부", default=False)
     
+    
+    def __str__(self):
+        return self.name
+    
 class Subscribe(models.Model):
-    subscriber = models.ForeignKey("user.User", verbose_name="구독자")
-    item = models.ForeignKey(Product, verbose_name="구독상품")
+    subscriber = models.ForeignKey("user.User", verbose_name="구독자", on_delete=models.CASCADE)
+    item = models.ForeignKey(Product, verbose_name="구독상품", on_delete=models.CASCADE)
     purchase_date = models.DateTimeField("구매일", auto_now_add=True)
     sub_start_date = models.DateTimeField("구독시작일", auto_now=True)
     sub_end_date = models.DateTimeField("구독종료일", auto_now=True)
+    
